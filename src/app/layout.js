31eb3fb6b5bb788ab654950fcfd27292fa056d88
Mael Donnart <maeldonnart@hotmail.fr>
2025-03-11 11:10:14 +0100

    Add Shadcn UI components Config shadcn

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,4 @@
+import { ThemeProvider } from '@/components/theme-provider'
 import '@/styles/globals.css'
 
 export const metadata = {
@@ -7,8 +8,17 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="en" suppressHydrationWarning>
+      <body>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   )
 }
